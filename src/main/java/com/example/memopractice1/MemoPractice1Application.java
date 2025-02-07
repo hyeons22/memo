528e8de32547a6c7f0a8c4ed8d2cb664413acaea
hyeon <ski0123@naver.com>
2025-02-07 19:06:17 +0900

    first commit

---
+++ src/main/java/com/example/memopractice1/MemoPractice1Application.java
@@ -0,0 +1,13 @@
+package com.example.memopractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoPractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoPractice1Application.class, args);
+    }
+
+}
